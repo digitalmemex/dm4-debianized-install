9cbd638a23401ab4d9773ccec595c11c9e83adff
Juergen Neumann <juergen@junes.eu>
2016-07-17 13:20:02 +0200

    a working prototype.

--- dm_install.sh
+++ dm_install.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Enter a valid version number, e.g. 4.8.2
 DM_VERSION="$1"
 if [ -z $1 ]; then
-    echo "Please enter version number!"
+    echo "ERROR! Please enter a valid version number, e.g. 4.8 or 4.8.2."
     exit 1
 fi
 
+# Are we root?
+if [ $( id -u ) != 0 ]; then
+    echo "ERROR! Please run the script as root or sudo $0."
+    exit 1
+fi
+
+
+# Variables
 ZEIT=$( date +%s )
 WORKDIR=/tmp/dm_installer_${ZEIT}
 BINDEST=/usr/share/deepamehta
@@ -13,24 +22,20 @@ VARDEST=/var/lib/deepamehta
 CACHEDIR=/var/cache/deepamheta
 LOGDIR=/var/log/deepamehta
 CONFDIR=/etc/deepamehta
+DOCDIR=/usr/share/doc/deepamehta
 
 LINKURL="http://download.deepamehta.de/deepamehta-${DM_VERSION}.zip"
+DEBURL="http://download.deepamehta.de/debian/dm-deb.zip"
 ZIPFILE="${WORKDIR}/deepamehta-${DM_VERSION}.zip"
 FILEDIR="${WORKDIR}/deepamehta-${DM_VERSION}"
-
-# Das soll später auch auf dem DM-Download Server
-DEBFILE="./dm-deb.zip"
-unzip -qq ${DEBFILE} -d ${WORKDIR}
-
-
-# Nicht vergessen: are we root?
 
 if [ ! -d ${WORKDIR} ]; then
     mkdir -p ${WORKDIR}
 fi
 
-wget -q ${LINKURL} -O ${ZIPFILE}
 
+# Fetch and unpack DeepaMehta
+wget -q ${LINKURL} -O ${ZIPFILE}
 if [ "$(  file -b ${ZIPFILE} )" == "empty" ]; then
     echo "ERROR! ${ZIPFILE} is emtpy."
     exit 1
@@ -39,38 +44,68 @@ elif [ "$(  file -b ${ZIPFILE} | grep "Zip archive data" )" != "" ]; then
 fi
 
 
-# Create destination directories and move the files
+# Fetch and unpack Debian Scripts
+wget -q ${DEBURL} -O ${WORKDIR}/dm-deb.zip
+if [ "$(  file -b ${WORKDIR}/dm-deb.zip )" == "empty" ]; then
+    echo "ERROR! ${WORKDIR}/dm-deb.zip is emtpy."
+    exit 1
+elif [ "$(  file -b ${WORKDIR}/dm-deb.zip | grep "Zip archive data" )" != "" ]; then
+    unzip -qq ${WORKDIR}/dm-deb.zip -d ${WORKDIR}
+fi
 
+
+# Remove Gogo-shell files
+rm ${FILEDIR}/bundle/org.apache.felix.gogo.*
+
+
+# Create destination directories and move the files
+mkdir -p ${CONFDIR}
 mkdir -p ${BINDEST}
 mkdir -p ${VARDEST}
 mkdir ${VARDEST}/deepamehta-db
 mkdir ${VARDEST}/deepamehta-filedir
 mkdir -p ${CACHEDIR}
 mkdir -p ${LOGDIR}
+mkdir -p ${DOCDIR}
 
+mkdir ${BINDEST}/bundle
+mkdir ${BINDEST}/bundle-deploy
+
+mv ${FILEDIR}/bundle/deepamehta-* ${BINDEST}/bundle-deploy/
+mv ${FILEDIR}/bundle/dm4* ${BINDEST}/bundle-deploy/
+mv ${FILEDIR}/bundle ${BINDEST}/
 mv ${FILEDIR}/bin ${BINDEST}/
-mv ${FILEDIR}/bundle ${BINDEST}/
-mv ${FILEDIR}/bundle-deploy ${BINDEST}/
-mv ${FILEDIR}/bundle ${BINDEST}/
+mv ${FILEDIR}/bundle-deploy/* ${BINDEST}/bundle-deploy/
 
 mv ${WORKDIR}/debian/default /etc/default/deepamehta
 mv ${WORKDIR}/debian/initd /etc/init.d/deepamehta
-mv ${WORKDIR}/debian/apache24 /etc/apache2/sites-available/deepamehta.conf.example
+mv ${WORKDIR}/debian/apache24 ${DOCDIR}/
 mv ${WORKDIR}/debian/logrotate /etc/logrotate.d/deepamehta
 mv ${WORKDIR}/debian/start ${BINDEST}/deepamehta.sh
+
 
 # Config files could be derived from originals with sed at some point
 mv ${WORKDIR}/debian/deepamehta.conf ${CONFDIR}/
 mv ${WORKDIR}/debian/deepamehta-logging.conf ${CONFDIR}/
 
+# Clean up
+rm -r ${FILEDIR}/bundle-deploy
+rm -r ${FILEDIR}/bundle-dev
+rm -r ${FILEDIR}/conf
+rm ${FILEDIR}/deepamehta-*
+
+# Move License and Readme to Docs
+mv ${FILEDIR}/* ${DOCDIR}/
+
+# Remove Workdir
+# rm -r ${WORKDIR}
+
 
 # Create system user
-
 useradd -s /usr/sbin/nologin -r -M -d ${VARDEST} deepamehta
 
 
 # Set owner and access rights
-
 chown -R deepamehta:root ${LOGDIR}
 chmod -R 550 ${LOGDIR}
 
@@ -83,11 +118,12 @@ chmod -R 550 ${VARDEST}
 chown -R root:root ${BINDEST}
 chmod -R 555 ${BINDEST}
 
-chmod 444 ${BINDEST}/bundle-deploy/*
+chmod 444 ${BINDEST}/bundle-deploy/about-this-directory.txt
 
 chown -R root:deepamehta ${CONFDIR}
 chmod 550 ${CONFDIR}
 
 # Update rc.d links
+update-rc.d deepamehta default
 
-update-rc.d deepamehta defaults
+#EOF
